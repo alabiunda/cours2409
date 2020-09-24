16bd576532e101b7813038dc72b5515f11cceb45
alabiunda <la_biunda.0108.student@ifosupwavre.be>
2020-09-24 21:02:16 +0200

    Error not working

--- src/main/java/com/example/demo/controller/index.java
+++ src/main/java/com/example/demo/controller/index.java
@@ -4,9 +4,12 @@ import com.example.demo.form.User;
 import lombok.Builder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Controller
 public class index {
@@ -27,7 +30,10 @@ public class index {
     }
 
     @PostMapping(value ="/create")
-    public String createUser(Model model, User user){
+    public String createUser(Model model, @Valid User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return"error";
+        }
         model.addAttribute("user", user);
         return "create_user";
     }
--- src/main/java/com/example/demo/form/User.java
+++ src/main/java/com/example/demo/form/User.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class User {
+    @NotEmpty
     private String lastName;
     private String firstName;
 }
